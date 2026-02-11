a5898d3e99e814013b2acf5af4c33a5a8d927604
Devendra Saini <30dsaini@gmail.com>
2026-02-10 21:13:12 -0800

    update

--- Config/Db.js
+++ Config/Db.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected ✅");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Atlas Connected ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌");
+    console.error(error.message);
+    process.exit(1); // stop server if DB fails
+  }
 };
 
-module.exports ={ connectDB};
+module.exports = { connectDB };
